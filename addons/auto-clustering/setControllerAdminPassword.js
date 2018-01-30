d08badbfb5cb9e101a3c4baa3e19682eb67f9eb8
SiryjVyiko <dz@hivext.net>
2018-01-30 17:48:53 +0200

    Update setControllerAdminPassword.js

--- addons/auto-clustering/setControllerAdminPassword.js
+++ addons/auto-clustering/setControllerAdminPassword.js
@@ -1,7 +1,7 @@
 import com.hivext.api.environment.Environment;
   
-var SOURCE_NODE = "node.cp.first.id";
-var TARGET_NODE = "node.extra.first.id";
+var SOURCE_NODE = "nodes.cp.first.id";
+var TARGET_NODE = "nodes.extra.first.id";
 var SESSION = hivext.local.getParam("session");
 var PASS_PATH = "/opt.wildfly/standalone/configuration/.pswd";
 
