265ac1718853345c5849cc09becc8c88b55f2ee0
SiryjVyiko <dz@hivext.net>
2018-01-30 16:48:11 +0200

    Update setControllerAdminPassword.js

--- addons/auto-clustering/setControllerAdminPassword.js
+++ addons/auto-clustering/setControllerAdminPassword.js
@@ -1,1 +1,13 @@
-return 0;
+import com.hivext.api.environment.Environment;
+  
+var SOURCE_NODE = "node.cp.first.id";
+var TARGET_NODE = "node.extra.first.id";
+var SESSION = hivext.local.getParam("session");
+var PASS_PATH = "/opt.wildfly/standalone/configuration/.pswd";
+
+var pswd = jelastic.env.file.Read(env.envName, SESSION, PASS_PATH, SOURCE_NODE);
+
+return {
+    result : 0,
+    pass : pass
+};
