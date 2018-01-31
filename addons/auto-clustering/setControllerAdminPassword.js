8c44bc02b323ea36fc5d5d7ecadd82366fece1b3
SiryjVyiko <dz@hivext.net>
2018-01-31 11:08:23 +0200

    Update setControllerAdminPassword.js

--- addons/auto-clustering/setControllerAdminPassword.js
+++ addons/auto-clustering/setControllerAdminPassword.js
@@ -1,16 +1,9 @@
-import com.hivext.api.environment.Environment;
-  
 var SOURCE_NODE = "${nodes.cp.first.id}";
 var TARGET_NODE = "${nodes.extra.first.id}";
-var SESSION = hivext.local.getParam("session");
-var PASS_PATH = "/opt.wildfly/standalone/configuration/.pswd";
+var PASS_PATH = "/opt/wildfly/standalone/configuration/.pswd";
 
-var pswd = jelastic.env.file.Read(${env.envName}, SESSION, PASS_PATH, null, null, SOURCE_NODE);
+var pswd = jelastic.env.file.Read(${env.envName}, session, PASS_PATH, null, null, SOURCE_NODE);
 
 var adminUserCredentials = "admin" + pswd
 
-jelastic.env.control.ExecCmdById(${env.envName}, SESSION, TARGET_NODE, toJSON([{ "command": "/opt/wildfly/bin/add-user.sh", "params": adminUserCredentials }]), null); 
-
-return {
-    result : 0,
-};
+jelastic.env.control.ExecCmdById(${env.envName}, session, TARGET_NODE, toJSON([{ "command": "/opt/wildfly/bin/add-user.sh", "params": adminUserCredentials }]), null); 
