0796a76f184a0c8c779764dcbf93fe84f543e420
SiryjVyiko <dz@hivext.net>
2018-01-31 18:17:11 +0200

    Update setControllerAdminPassword.js

--- addons/auto-clustering/setControllerAdminPassword.js
+++ addons/auto-clustering/setControllerAdminPassword.js
@@ -4,7 +4,7 @@ import com.hivext.api.utils.Random;
 
 var SOURCE_NODE = "${settings.masterId}";
 
-if if (${nodes.ctrl.first.id} != "${nodes.ctrl.first.id") {
+if (${nodes.ctrl.first.id} != "${nodes.ctrl.first.id") {
     var TARGET_NODE = "${nodes.ctrl.first.id}";
 }
 
