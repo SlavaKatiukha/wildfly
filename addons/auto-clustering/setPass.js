4f64dc5b5650d951bd4233afdba3a3dc1093881f
SiryjVyiko <dz@hivext.net>
2018-02-01 18:22:13 +0200

    Create setPass.js

---
+++ addons/auto-clustering/setPass.js
@@ -0,0 +1,20 @@
+import com.hivext.api.core.utils.Transport;
+import com.hivext.api.development.Scripting;
+import com.hivext.api.utils.Random;
+
+var SOURCE_NODE = "${settings.masterId}";
+var TARGET_NODE = "${this.ctrlNodeId}";
+var PASS_PATH = "/opt/wildfly/standalone/configuration/.pswd" ;
+
+var pswd = jelastic.env.file.Read('${env.envName}', session, PASS_PATH, null, null, SOURCE_NODE);
+
+var resetPasswordCmd = "/opt/wildfly/bin/add-user.sh" ;
+
+if (pswd.result != 0) {
+    return pswd;
+}
+
+var adminUserCredentials = "admin " + pswd.body.replace(/\n/g, '');
+
+jelastic.env.control.ExecCmdById('${env.envName}', session, SOURCE_NODE, toJSON([{ "command": "/usr/bin/rm -f", "params": PASS_PATH }]), false, "root");
+return jelastic.env.control.ExecCmdById('${env.envName}', session, TARGET_NODE, toJSON([{ "command": resetPasswordCmd, "params": adminUserCredentials }]), false, "root");
