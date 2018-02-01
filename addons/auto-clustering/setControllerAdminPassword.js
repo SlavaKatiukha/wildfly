159d26e2ddf24e120660a7b21641b1d398fc5f27
SiryjVyiko <dz@hivext.net>
2018-02-01 12:02:38 +0200

    Update setControllerAdminPassword.js

--- addons/auto-clustering/setControllerAdminPassword.js
+++ addons/auto-clustering/setControllerAdminPassword.js
@@ -12,9 +12,11 @@ var SOURCE_NODE = "${settings.masterId}";
     var TARGET_NODE = "${nodes.ctrl.first.id}";
 //}
 
-var PASS_PATH = "/opt/wildfly/standalone/configuration/.pswd";
+var PASS_PATH = "/opt/wildfly/standalone/configuration/.pswd" ;
 
 var pswd = jelastic.env.file.Read('${env.envName}', session, PASS_PATH, null, null, SOURCE_NODE);
+
+var resetPasswordCmd = "/opt/wildfly/bin/add-user.sh" ;
 
 if (pswd.result != 0) {
     return pswd;
@@ -23,4 +25,4 @@ if (pswd.result != 0) {
 var adminUserCredentials = "admin " + pswd.body;
 
 jelastic.env.control.ExecCmdById('${env.envName}', session, SOURCE_NODE, toJSON([{ "command": "/usr/bin/rm -f", "params": PASS_PATH }]), false, "root");
-return jelastic.env.control.ExecCmdById('${env.envName}', session, TARGET_NODE, toJSON([{ "command": "/usr/bin/sh /opt/wildfly/bin/add-user.sh", "params": adminUserCredentials }]), false, "root");
+return jelastic.env.control.ExecCmdById('${env.envName}', session, TARGET_NODE, toJSON([{ "command": resetPasswordCmd, "params": adminUserCredentials }]), false, "root");
