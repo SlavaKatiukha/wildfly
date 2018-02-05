5b0158af750d795622c7399221d3292ee1ccfda1
SiryjVyiko <dz@hivext.net>
2018-02-05 10:44:32 +0200

    Update setAdminPassword.js

--- scripts/setAdminPassword.js
+++ scripts/setAdminPassword.js
@@ -8,12 +8,11 @@ var PASS_PATH = "/opt/wildfly/standalone/configuration/.pswd" ;
 
 var pswd = jelastic.env.file.Read('${env.envName}', session, PASS_PATH, null, null, SOURCE_NODE);
 
-if (resp.result != 0) return resp;
+if (pswd.result != 0) return resp;
 
 var resetPasswordCmd = "/opt/wildfly/bin/add-user.sh" ;
 
 var adminUserCredentials = "admin " + pswd.body.replace(/\n/g, '');
 
 jelastic.env.control.ExecCmdById('${env.envName}', session, SOURCE_NODE, toJSON([{ "command": "/usr/bin/rm -f", "params": PASS_PATH }]), false, "root");
-if (resp.result != 0) return resp;
 return jelastic.env.control.ExecCmdByGroup('${env.envName}', session, "ctrl", toJSON([{ "command": resetPasswordCmd, "params": adminUserCredentials }]), false, false, "root");
