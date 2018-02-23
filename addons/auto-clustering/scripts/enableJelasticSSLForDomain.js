7584079843cd873b233b5d6e143d0609443691b2
SiryjVyiko <dz@hivext.net>
2018-02-23 17:11:43 +0200

    Update enableJelasticSSLForDomain.js

--- addons/auto-clustering/scripts/enableJelasticSSLForDomain.js
+++ addons/auto-clustering/scripts/enableJelasticSSLForDomain.js
@@ -8,4 +8,4 @@ if (domainEnvInfo.result != 0) return resp;
 
 domainEnvInfo.env.sslState = true;
 
-return jelastic.env.control.ChangeTopology('${env.envName}', session, '' , domainEnvInfo.env, domainEnvInfo.nodes);
+return jelastic.env.control.ChangeTopology('${env.envName}', session, '1519393031834;changetopology;28919757aa80fcb9c06905fe6eddbe4c;env-ww' , domainEnvInfo.env, domainEnvInfo.nodes);
